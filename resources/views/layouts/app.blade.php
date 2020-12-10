0088bb8854969bbf8262d92260de738fa322acc8
Kevin Loquencio <kevin.loquencio@gmail.com>
2020-12-10 21:54:55 +0800

    Remove style from main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,23 +15,7 @@
     <link href="{{ asset('css/reactions.css') }}" rel="stylesheet">
 
     @yield('header')
-
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-
-    </style>
+    
 </head>
 
 <body>
